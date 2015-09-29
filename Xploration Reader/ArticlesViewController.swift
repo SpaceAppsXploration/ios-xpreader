7e03c95d32217a2481091981df25975581828a48
Andrea Cerra <andrea.cerra@me.com>
2015-09-29 12:11:01 +0200

    Other changes in storyboard, add title to view controllers and set a white status bar

---
+++ Xploration Reader/ArticlesViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ArticlesViewController.swift
+//  Xploration Reader
+//
+//  Created by Andrea Cerra on 29/09/15.
+//  Copyright © 2015 Andrea Cerra. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
